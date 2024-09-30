3cdfbc44fc93d28b65d02a17f38a86c1a30a32f3
yvromero <yvonne.romero@uds.edu.py>
2024-09-30 18:19:32 -0400

    Initial commit on feature-1

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+
+
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
