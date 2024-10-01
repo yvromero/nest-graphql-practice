eb5463342870bcc7357858a0579e35d47298e1ac
yvromero <yvonne.romero@uds.edu.py>
2024-10-01 00:57:25 -0400

    Add exercises with queries using args

--- src/hello-world/hello-world.resolver.ts
+++ src/hello-world/hello-world.resolver.ts
@@ -1,4 +1,4 @@
-import { Float, Int, Query, Resolver } from '@nestjs/graphql';
+import { Args, Float, Int, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class HelloWorldResolver {
@@ -13,8 +13,16 @@ export class HelloWorldResolver {
         return Math.random() * 100;
     }
 
-    @Query( () => Int, { name: 'randomFromZeroTo' } )
-    getRandomFromZeroTo(): number {
-        return Math.floor(Math.random() * 10 );
+    // @Query( () => Int, { name: 'randomFromZeroTo' } )
+    // getRandomFromZeroTo(): number {
+    //     return Math.floor(Math.random() * 10 );
+    // }
+
+    // Args
+    @Query( () => Int, { name: 'randomFromZeroTo', description: 'From Zero to argument TO (default 6)' } )
+    getRandomFromZeroTo( 
+        @Args('to', { type: () => Int, nullable: true } ) to: number = 6
+    ): number {
+        return Math.floor(Math.random() * to );
     }
 }
